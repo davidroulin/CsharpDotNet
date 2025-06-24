d9d3d89e1d41e69791bbc76177f70fa6f5863a29
David Roulin <freelance@davidroulin.ch>
2025-06-24 07:52:18 +0200

    snippet un peu nul

---
+++ ConsoleApp1/AsyncAwait.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+partial class Program
+{
+    public void MethodeBloquante(int dureeMillis = 2000)
+    {
+
+        //Thread.Sleep(dureeMillis);
+        //Task.Delay() 
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿partial class Program
+{
+
+    private static Program _singleton;
+
+    public static Program Instance
+    {
+        get
+        {
+            _singleton ??= new Program();
+            return _singleton;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("coucou");
+        Instance.MethodeBloquante(4000);
+    }
+}
